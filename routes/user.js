15de0e0081dd2d5793373f9fb0c79c1cbbceb38e
reddy1289 <inandipa@uncc.edu>
2016-12-08 20:48:43 -0500

    as

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,11 @@ login.get('/', function(req, res, next) {
     console.log('get');
 });
 
+login.get('/signup', function(req, res, next) {
+    res.render('user/signup');
+    console.log('signup');
+});
+
 login.get('/login', function(req, res, next) {
     console.log('login');
     res.render('user/services');
@@ -16,22 +21,43 @@ login.get('/login', function(req, res, next) {
 
 
 login.post('/login', function(req, res, next) {
-    var emailId = req.body.username;
-    var password = req.body.password;
+    var emailId = req.body.Username;
+    var password = req.body.Password;
 
        connection.query("SELECT * from Customer where emailId = '" + emailId + "' and customerPassword = '" + password + "'", function (err, rows) {
            if (!err) {
                if (rows.length == 0) {
-                   res.render('pages/login', {error: "incorrect credentials"});
+                   res.render('user/login', {error: "incorrect credentials"});
                } else {
                    console.log('The solution is: ', rows);
                    req.session.user = {userData : rows};
-                   res.render('pages/home');
+                   res.render('user/index');
                }
            } else {
                console.log(err);
            }
        });
+});
+
+
+login.post('/signup', function(req, res, next) {
+    var emailId = req.body.username;
+    var password = req.body.password;
+    //INSERT into DbProject_Cebordine.Customer values
+    //('131','indra@gmail.com','kittansett street d NC','afg','Santosh','Peesu','Reddy');
+    connection.query("INSERT into DbProject_Cebordine.Customer values('" + emailId + "' and customerPassword = '" + password + "'", function (err, rows) {
+        if (!err) {
+            if (rows.length == 0) {
+                res.render('user/signup', {error: "incorrect credentials"});
+            } else {
+                console.log('The solution is: ', rows);
+                req.session.user = {userData : rows};
+                res.render('user/index');
+            }
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 module.exports = login;
